7b30bd4dcfbe5b034e8a4d217464b283f74c9a14
RihabMhd <rihabmahdi19@gmail.com>
2026-09-08 10:16:43 +0100

    chanllenge 1 done

---
+++ challenge1.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+tab1=np.random.randint(5000,size=10)
+print(tab1)
+
+print("Shape:",tab1.shape)
+print("Dimensions:",tab1.ndim)
+print("Size:",tab1.size)
+print("Type:",tab1.dtype)
+
+print(tab1[0])
+print(tab1[-1])
+print(min(tab1))
+print(max(tab1))
+print(np.mean(tab1))
