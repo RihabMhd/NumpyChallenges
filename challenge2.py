8679a23f4981103351eed6f448d640a509e0618c
RihabMhd <rihabmahdi19@gmail.com>
2026-09-08 10:25:42 +0100

    chanllenge 2 done

---
+++ challenge2.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+jours=np.arange(10)
+
+temperatures=np.linspace(5,12)
+
+prix=np.zeros(7)
+
+ids=np.ones(7)
+
+prices=np.full([3,3],21)
+
+print(jours)
+print(temperatures)
+print(prix)
+print(ids)
+print(prices)
+
+print(jours.dtype)
+print(temperatures.dtype)
+print(prix.dtype)
+print(ids.dtype)
+print(prices.dtype)
+
